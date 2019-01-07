187a3a0f0eee910961ea0e856232e31d065b2abc
EC2 Default User <ec2-user@ip-172-31-16-15.us-east-2.compute.internal>
2019-01-07 22:32:19 +0000

    CHanged new medication form to have dropdown

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,6 +1,6 @@
 class Medication < ApplicationRecord
   has_many :times, :class_name => 'MedicationTime'
   validates :name, :presence => true, :length => { :minimum => 3 }
-  validates :unit, :presence => true, :inclusion => { :in => [ 'mg', 'mL', 'tabs', 'caps' ] }
+  validates :unit, :presence => true, :inclusion => { :in => [ 'mg', 'mL', 'tabs', 'caps', 'pills' ] }
   validates :quantity, :presence => true, :numericality => { :greater_than => 0 }
 end
